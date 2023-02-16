6036229c2031de619f4bef3c385af664cdddc091
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-16 10:13:06 +0000

    Created initial Input structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React, {FC, useState} from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  font-size: 18px;
+  padding: 10px;
+  margin: 10px;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
 
 const App:FC = () => {
   return (
-    <div className="App">
-   
+    <>
+    {/* input container */}
+    <div>
+      <form>
+        <Input></Input>
+        <Input></Input>
+      </form>
     </div>
+    {/* input container */}
+    {/* results container */}
+    {/* results container */}
+  </>
   );
 }
 
