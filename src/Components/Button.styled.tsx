e5fd42fbb5f1d58df032bdede25a08d9494872a0
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-17 14:32:44 +0000

    Styled button

--- src/Components/Button.styled.tsx
+++ src/Components/Button.styled.tsx
@@ -2,5 +2,13 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   width: 200px;
-  height: 60px;
+  height: 50px;
+  border-radius: 40px;
+  cursor: pointer;
+  background-image: linear-gradient(
+    30deg,
+    rgb(1, 114, 175) 25%,
+    rgb(1, 114, 175) 0%,
+    rgb(116, 254, 189)
+  );
 `;
