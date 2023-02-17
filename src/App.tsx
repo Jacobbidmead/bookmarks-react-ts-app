1bf29190c3388d21510368fda829c6fc77a1ebab
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-17 11:04:51 +0000

    Added initial state for editing links

--- src/App.tsx
+++ src/App.tsx
@@ -17,24 +17,27 @@ interface Link {
 }
 
 const App:FC = () => {
+
 // Sets inital state of links to empty array
   const [links, setLinks] = useState<Link[]>([])
+
+// Sets initial state for the edit links functionality 
+  const [editLinks, setEditLinks] = useState<number | null>(null);
+  
  
 
   // Function to change state of links
+
   const addLink = (url:string, text:string) => {
     if (!url || !text) {
       console.log('Url and Text fields are required');
       return;
     }
-    
     setLinks([...links, {url, text}])
-
-// sets the state back to original state - needs to be fixed
-
-
-   
   }
+
+
+
 
   // Create a function that changes the state of links
 
@@ -44,10 +47,27 @@ const App:FC = () => {
     const url = (form.elements.namedItem("url") as HTMLInputElement).value;
     const text = (form.elements.namedItem("text") as HTMLInputElement).value;
     addLink(url, text)
-    event.currentTarget.reset();
 
-  
+
+
+    if (editLinks !== null) {
+      // If the editLinks state is not null, it means we're in edit mode
+      // So we update the corresponding link instead of adding a new one
+      const newLinks = [...links];
+      newLinks[editLinks].url = url;
+      newLinks[editLinks].text = text;
+      setLinks(newLinks);
+      setEditLinks(null);
+    } else {
+      // Otherwise, we're in add mode, so we add the new link
+      addLink(url, text);
+    }
+
+    
+    event.currentTarget.reset();
   }
+
+
 
   // Create a function to remove link
 
@@ -55,7 +75,6 @@ const App:FC = () => {
     const newLinks = [...links];
     newLinks.splice(index, 1);
     setLinks(newLinks);
-    
   };
 
   // Function to clear all links
@@ -65,6 +84,16 @@ const App:FC = () => {
   };
 
 
+
+
+// Function to save edited link
+const saveEdit = (index: number, url: string, text: string) => {
+  const newLinks = [...links];
+  newLinks[index] = { url, text };
+  setLinks(newLinks);
+  setEditLinks(-1);
+};
+ 
 
   return (
     <>
@@ -83,15 +112,25 @@ const App:FC = () => {
     {/* Map links on form submit*/}
 
     
-{links.map((link, index) => (  <div key={index}><a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a><button onClick={() => removeLink(index)}>Remove</button></div> ))}
+{links.map((link, index) => (  
+<div key={index}>
+<a href={link.url} target="_blank" rel="noopener noreferrer">{link.text}</a>
+<button onClick={() => removeLink(index)}>Remove</button> 
+<button >Edit</button></div> ))}
 
     {/* results container end*/}
     {/* clear button */}
 
-    <div><Button onClick={clearLinks}>Clear all</Button></div>
+<div><Button onClick={clearLinks}>Clear all</Button></div>
     {/* clear button end */}
   </>
   );
 }
+
+
+
+
+
+
 
 export default App;
